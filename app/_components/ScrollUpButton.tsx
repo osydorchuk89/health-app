9d9e51bb2362fa404288fc8753d313d05adaf399
Oleksii Sydorchuk <oleksii.sydorchuk@gmail.com>
2025-01-19 18:05:07 +0100

    edited scroll up button

--- app/_components/ScrollUpButton.tsx
+++ app/_components/ScrollUpButton.tsx
@@ -13,7 +13,7 @@ export const ScrollUpButton = () => {
     useEffect(() => {
         const toggleVisible = () => {
             const { scrollHeight, scrollTop } = document.documentElement;
-            if (scrollTop > scrollHeight * 0.8) {
+            if (scrollTop > scrollHeight * 0.2) {
                 setVisible(true);
             } else {
                 setVisible(false);
@@ -30,14 +30,16 @@ export const ScrollUpButton = () => {
     };
 
     const baseIconClassName =
-        "p-7 border border-pale rounded-full z-10 cursor-pointer bg-primary active:bg-accent";
+        "p-3 lg:p-7 border border-pale rounded-full z-10 cursor-pointer active:bg-accent";
     const mouseDeviceIconClassName =
         baseIconClassName + " hover:border-accent hover:shadow-custom";
 
     return (
         <div className="w-full flex justify-end items-center fixed bottom-10 px-4 md:px-10 lg:px-20">
             <div
-                className={isTouchDevice ? baseIconClassName : mouseDeviceIconClassName}
+                className={
+                    isTouchDevice ? baseIconClassName : mouseDeviceIconClassName
+                }
                 onClick={scrollToTop}
                 onMouseEnter={() => setIconStatus("hovered")}
                 onMouseDown={() => setIconStatus("clicked")}
